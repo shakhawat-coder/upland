c39c12f2aaf8f11723f9295e2521c357f508676a
shakhawat <shakhawathossen188@gmail.com>
2023-12-19 20:21:31 +0600

    testimony section and trial section design

--- js/script.js
+++ js/script.js
@@ -2,10 +2,40 @@ $(document).ready(function(){
     $('.slider_wrapper').slick({
         centerMode: true,
         centerPadding: '60px',
-        prevArrow: ".prev",
-        nextArrow: ".next",
+        prevArrow: ".slider .prev",
+        nextArrow: ".slider .next",
         slidesToShow: 3,
         autoplay: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+  });
+$(document).ready(function(){
+    $('.testimony_slider_wrapper').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      prevArrow: ".testimony_slider .prev",
+      nextArrow: ".testimony_slider .next",
         responsive: [
           {
             breakpoint: 768,
